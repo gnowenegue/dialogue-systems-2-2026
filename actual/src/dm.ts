bce7410a1a2616164171bc832bc90e6bbbc0702c
Eugene Wong <gnow.enegue@gmail.com>
2026-09-13 17:45:19 +0200

    Duplicate starter folder.

---
+++ actual/src/dm.ts
@@ -0,0 +1,172 @@
+import { assign, createActor, fromPromise, setup } from "xstate";
+import { Settings, speechstate } from "speechstate";
+import { KEY } from "./credentials";
+import { DMContext, DMEvents } from "./types";
+import OpenAI from "openai";
+
+const REGION = "<YOUR_REGION>";
+
+const openai = new OpenAI({
+  baseURL: "http://localhost:11434/v1/",
+  apiKey: "ollama",
+  dangerouslyAllowBrowser: true,
+});
+
+const azureCredentials = {
+  endpoint: `https://${REGION}.api.cognitive.microsoft.com/sts/v1.0/issuetoken`,
+  key: KEY,
+};
+
+/** backup: Azure access via FLoV proxy
+const azureProxyCredentials = {
+  proxyUrl: "https://rndserv.flov.gu.se:4000/api/token",
+  key: "",
+  };
+*/
+
+const settings: Settings = {
+  azureCredentials: azureCredentials,
+  azureRegion: REGION,
+  asrDefaultCompleteTimeout: 0,
+  asrDefaultNoInputTimeout: 5000,
+  locale: "en-US",
+  ttsDefaultVoice: "en-US-DavisNeural",
+  bargeIn: false,
+};
+
+interface GrammarEntry {
+  person?: string;
+  day?: string;
+  time?: string;
+}
+
+const grammar: { [index: string]: GrammarEntry } = {
+  vlad: { person: "Vladislav Maraev" },
+  bora: { person: "Bora Kara" },
+  tal: { person: "Talha Bedir" },
+  tom: { person: "Tom Södahl Bladsjö" },
+  monday: { day: "Monday" },
+  tuesday: { day: "Tuesday" },
+  "10": { time: "10:00" },
+  "11": { time: "11:00" },
+};
+
+function isInGrammar(utterance: string) {
+  return utterance.toLowerCase() in grammar;
+}
+
+const dmMachine = setup({
+  types: {
+    /** you might need to extend these */
+    context: {} as DMContext,
+    events: {} as DMEvents,
+  },
+  actions: {
+    /** define your actions here */
+    "spst.speak": ({ context }, params: { utterance: string }) =>
+      context.spstRef.send({
+        type: "SPEAK",
+        value: {
+          utterance: params.utterance,
+        },
+      }),
+    "spst.listen": ({ context }) =>
+      context.spstRef.send({
+        type: "LISTEN",
+      }),
+  },
+  actors: {},
+}).createMachine({
+  context: ({ spawn }) => ({
+    spstRef: spawn(speechstate, { input: settings }),
+    lastResult: null,
+  }),
+  id: "DM",
+  initial: "Prepare",
+  states: {
+    Prepare: {
+      entry: ({ context }) => context.spstRef.send({ type: "PREPARE" }),
+      on: { ASRTTS_READY: "WaitToStart" },
+    },
+    WaitToStart: {
+      on: { CLICK: "Greeting" },
+    },
+    Greeting: {
+      initial: "Prompt",
+      on: {
+        LISTEN_COMPLETE: [
+          {
+            target: "CheckGrammar",
+            guard: ({ context }) => !!context.lastResult,
+          },
+          { target: ".NoInput" },
+        ],
+      },
+      states: {
+        Prompt: {
+          entry: { type: "spst.speak", params: { utterance: `Hello world!` } },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        NoInput: {
+          entry: {
+            type: "spst.speak",
+            params: { utterance: `I can't hear you!` },
+          },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        Ask: {
+          entry: { type: "spst.listen" },
+          on: {
+            RECOGNISED: {
+              actions: assign(({ event }) => {
+                return { lastResult: event.value };
+              }),
+            },
+            ASR_NOINPUT: {
+              actions: assign({ lastResult: null }),
+            },
+          },
+        },
+      },
+    },
+    CheckGrammar: {
+      entry: {
+        type: "spst.speak",
+        params: ({ context }) => ({
+          utterance: `You just said: ${context.lastResult![0].utterance}. And it ${
+            isInGrammar(context.lastResult![0].utterance) ? "is" : "is not"
+          } in the grammar.`,
+        }),
+      },
+      on: { SPEAK_COMPLETE: "Done" },
+    },
+    Done: {
+      on: {
+        CLICK: "Greeting",
+      },
+    },
+  },
+});
+
+const dmActor = createActor(dmMachine, {}).start();
+
+dmActor.subscribe((state) => {
+  console.group("State update");
+  console.log("State value:", state.value);
+  console.log("State context:", state.context);
+  console.groupEnd();
+});
+
+export function setupButton(element: HTMLButtonElement) {
+  element.addEventListener("click", () => {
+    dmActor.send({ type: "CLICK" });
+  });
+  dmActor.subscribe((snapshot) => {
+    const meta: { view?: string } = Object.values(
+      snapshot.context.spstRef.getSnapshot().getMeta(),
+    )[0] || {
+      view: undefined,
+    };
+    element.innerHTML = `${meta.view}`;
+  });
+}
